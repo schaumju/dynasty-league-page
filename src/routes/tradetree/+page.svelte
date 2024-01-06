49753b4672bfa825b5b33f2c133980ca393fd6d5
Justin Schaumberger <jes083@bucknell.edu>
2024-01-05 21:59:56 -0800

    Create +page.svelte

---
+++ src/routes/tradetree/+page.svelte
@@ -0,0 +1,10 @@
+<div class="tradetree">
+    <h1 class="noUnderscore">LOC Trade Tree</h1>
+
+    <iframe src='https://app.mural.co/embed/710a5dca-2b4f-41dd-ba6b-5d4557be3c1b'
+      width='100%'
+      height='480px'
+      style='min-width: 640px; min-height: 480px; background-color: #f4f4f4; border: 1px solid #efefef'
+      sandbox='allow-same-origin allow-scripts allow-modals allow-popups allow-popups-to-escape-sandbox'/>
+
+</div>

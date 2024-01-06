111e2f0120727d342b6476c8dd7e7efa32ab33b4
Justin Schaumberger <jes083@bucknell.edu>
2024-01-05 21:02:17 -0800

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -224,7 +224,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": null, // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "https://github.com/schaumju/dynasty-league-page/blob/master/static/managers/allen.PNG", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/allen.PNG", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
